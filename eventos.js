cc72f6df062b58f59736383c4adb91798eb4dd87
donSebamarquez <sebamarquezblanco@gmail.com>
2025-08-31 19:40:33 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 let div = document.querySelector(".div")
-
+let btn = document.querySelector(".btn")
 div.addEventListener("click",function(){
     alert("Hola! Soy el div");
+    
+})
+
+btn.addEventListener("click", ()=>{
+    alert("Hola! Soy el boton");
+    event.stopPropagation();
 })
